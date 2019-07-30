60b3660bb0baa212394f41c205ffbfa27d5940fa
megan schulte <megan.schulte.pdx@gmail.com>
2019-07-29 22:35:19 -0700

    add rest of firebase list items to fetch

--- components/MovieComponent.js
+++ components/MovieComponent.js
@@ -13,9 +13,23 @@ export default class MovieComponent extends Component {
         {this.props.movies.map((movie, index) => {
           return (
             <View key={index}>
-              <Text style={styles.movieText}>{movie.title}</Text>
-              <Text style={styles.movieText}>{movie.release}</Text>
-              <Text style={styles.movieText}>{movie.format}</Text>
+              <Text>{movie.image_url}</Text>
+
+              <Text style={styles.title}>Title  |    {movie.title}</Text>
+
+              <Text style={styles.release}>Release Year  |   {movie.release}</Text>
+
+              <Text style={styles.literature}>Screenplay written by  |   {movie.literature}</Text>
+
+              <Text style={styles.format}>Movie or TV Mini-series  |   {movie.format}</Text>
+
+              <Text style={styles.directed}>Directe By  |   {movie.directedBy}</Text>
+
+              <Text style={styles.screenplay}>Screenplay written by  |   {movie.screenplay}</Text>
+
+              <Text style={styles.synopsis}>Screenplay written by  |   {movie.synopsis}</Text>
+
+              <Text style={styles.funfact}>Screenplay written by  |   {movie.funfact}</Text>
               
               </View>
           );
@@ -32,9 +46,58 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     color: 'white'
   },
-  movieText: {
-    fontSize: 24,
+  title: {
+    fontSize: 14,
     textAlign: 'center',
-    color: 'white'
-  }
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  release: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  release: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  format: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  directed: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  screenplay: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  literature: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  synopsis: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
+  funfact: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: 'white',
+    textTransform: 'uppercase'
+  },
 });
