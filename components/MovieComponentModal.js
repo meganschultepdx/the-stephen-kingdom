d214820f2417f0af62d9e80d3cb462df54c53e98
Student <example@example.com>
2019-07-31 17:34:33 -0700

    set show hide on movie descriptions

--- components/MovieComponentModal.js
+++ components/MovieComponentModal.js
@@ -1,36 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, TouchableHighlight, Button, LayoutAnimation, NativeModules } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, Modal, TouchableHighlight, Button } from 'react-native';
 import PropTypes from 'prop-types';
 import { whileStatement } from '@babel/types';
 
-const { UIManager } = NativeModules;
 
-UIManager.setLayoutAnimationEnabledExperimental &&
-  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default class MovieComponent extends Component {
   static propTypes = {
     movies: PropTypes.array.isRequired
   };
 
-  constructor(){
-      super();
-      this.state = {
-          status: false
-      }
+  state = {
+    modalVisible: false,
   }
 
-  ShowHideMovieData = () =>{
-      if(this.state.status === true)
-      {
-          this.setState({status: false})
-      }
-      else
-      {
-          this.setState({status: true})
-      }
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
   }
-
 
   render() {
 
@@ -42,14 +28,24 @@ export default class MovieComponent extends Component {
             <ScrollView key={index}>
               
                 <TouchableHighlight
-                  onPress={this.ShowHideMovieData}>
+                  onPress={() => {
+                    this.setModalVisible(true);
+                  }}>
                   <Image 
                     style={{ width: 130, height: 190 }} source={{ uri: this.props.movies[index].image_url }} />
                 </TouchableHighlight>
 
-                <View style={styles.MovieDataContainer}>
-                { this.state.status ? 
-                <View>
+              <Modal
+                style={styles.modal}
+                transparent={false}
+                backdropColor = {'black'}
+                bakcdropOpacity = {4}
+                marginTop={50}
+                animationType="slide"
+                  visible={this.state.modalVisible}
+                onRequestClose={() => {
+                  this.setModalVisible(false);
+                }} >
                 <Text style={styles.title}>Title  |    {this.props.movies[index].title}</Text>
 
                 <Text style={styles.release}>Release Year  |   {this.props.movies[index].release}</Text>
@@ -65,11 +61,15 @@ export default class MovieComponent extends Component {
                 <Text style={styles.synopsis}>Synopsis  |   {this.props.movies[index].synopsis}</Text>
 
                 <Text style={styles.funfact}>Fun Filming Fact  |   {this.props.movies[index].funfact}</Text>
-                </View>
-                : null }
-                
-                </View>
-                
+
+                <Button onPress={() => {
+                  console.log("press");
+                  this.setModalVisible(!this.state.modalVisible);
+                }} title="close" style={styles.closeModal}/>
+
+              </Modal>
+
+
             </ScrollView>
           );
         })}
@@ -80,70 +80,67 @@ export default class MovieComponent extends Component {
 
 const styles = StyleSheet.create({
 
-MovieDataContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    margin: 30,
-},
-
+  modal: {
+    marginTop: 100,
+  },
   moviesList: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-around',
-    
+    color: 'black'
   },
   title: {
     fontSize: 14,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   release: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   release: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   format: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   directed: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   screenplay: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   literature: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   synopsis: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
   funfact: {
     fontSize: 12,
     textAlign: 'center',
-    color: 'white',
+    color: 'black',
     textTransform: 'uppercase'
   },
 });
