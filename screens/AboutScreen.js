58521f10b92a205f1a538856ebd8e9e74a72b1f3
megan schulte <megan.schulte.pdx@gmail.com>
2019-07-31 22:36:05 -0700

    add content and styling to about screen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -10,6 +10,7 @@ import {
   View,
   Dimensions
 } from 'react-native';
+import FadeInView from '../components/FadeInView';
 
 
 const dimensions = Dimensions.get('window');
@@ -26,12 +27,70 @@ export default function AboutScreen() {
         <View style={styles.aboutContainer}>
           <Image
             source={
-                require('../assets/images/movie-collage.jpg')
+              require('../assets/images/creepshow.jpeg')
             }
-            style={styles.aboutImage}/>
+            style={styles.aboutImage} />
+          <FadeInView>
+            <Text style={styles.about}>about the Kingdom</Text>
+          </FadeInView>
+          <Text style={styles.why}>{` 
+Awhile ago I thought it would be a fun idea 
+to work my way through the vast list of
+Stephen King movies (I never made it) and 
+starting to do that just made me more
+interested in his actual 
+role (or non-roll) in the list of
+movies that are considered tied to him.
+
+The Stephen Kingdom is my effort to 
+compile a list of movies considered to be 
+one of Kings's, and then let you know how 
+he is associated with it (sprinkled with a 
+few fun filming facts along the way). 
+`}</Text>
+          <Text style={styles.enjoy}>
+          Enjoy!</Text>
+          <FadeInView>
+            <Text style={styles.about}>about Stephen King</Text>
+          </FadeInView>
+          <Text style={styles.why}>{`
+          - He was hit by a car, then bought the 
+          car that hit him
+
+          - He played in a band from 1992 to 
+          2012 with Amy Tan, Dave Barry, Mitch 
+          Albom, Barbara Kingsolver, Matt Groening
+          and Ridley Pearson, each members 
+          at some point. 
+
+          - He got his start publishing his short
+          stories in porn magazines
+
+          - He wrote a musical with 
+          John Mellancamp
+          
+          - He's done a bit of coke over the years
+          hahaha, duh
+
+          - He barely remembers writing Cujo (see
+           previous fact)
+
+          - He's published books under the alias 
+          Richard Bachman
+
+          - as a kid, his friend was struck and 
+          killed by a train
+
+          - He originally threw the beginning pages 
+          of Carrie away thinking it was trash, 
+          luckily his wife pulled it out and 
+          convinced him to keep going with it
+
+          - King suffers from triskaidekaphobia
+          `}</Text>
         </View>
 
-      
+
       </ScrollView>
 
     </View>
@@ -41,28 +100,42 @@ export default function AboutScreen() {
 AboutScreen.navigationOptions = {
   header: null,
 };
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
   },
-  
-  contentContainer: {
-   
-  },
+
   aboutContainer: {
     alignItems: 'center',
-    
     marginBottom: 20,
   },
-  //top image of stephen king
+
   aboutImage: {
     width: imageWidth,
-    resizeMode: 'contain',
-    marginTop: 20,
+    marginTop: 50,
+    borderRadius: 4,
   },
+  about: {
+    color: '#bf312d',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontFamily: 'eighties-horror',
+    fontSize: 24,
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  why: {
+    color: 'lightgrey',
+    fontSize: 15,
+    marginTop: -20,
+  },
+  enjoy: {
+      color: 'lightgrey',
+      fontSize: 15,
+      marginTop: -20,
+      textAlign: 'center',
+    }
   
 
 });
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -2,11 +2,8 @@ import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
   Image,
-  Platform,
   ScrollView,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
   Dimensions,
   Animated
